ce6e3f9bfcc440ffc7df60eb975fecad7dfc864e
Meet Patel <meet.patel@azilen.com>
2025-09-08 13:02:03 +0530

    small code update

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -2,7 +2,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features",
+        features = "src/test/resources/features/Dashboard.feature",
         glue = {"stepdefinitions", "hooks"},
         plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"},
         monochrome = true
